9be8dc75609dc95af0a536d28e56165f46edd050
황인태 <ghkddlsxo55@gmail.com>
2022-02-24 23:44:25 +0900

    오류의 이유를 찾아냄 싱글톤인데 생성자 소멸자를 정의해서 그럼

--- windowsAPI/CSceneManager.h
+++ windowsAPI/CSceneManager.h
@@ -1,18 +1,20 @@
 #pragma once
 
-//TODO : 이쪽 문제인데
+
 class CScene;
 
 class CSceneManager
 {
 private:
+	//TODO : 싱글톤으로 선언했는데 생성자랑 소멸자 넣어서 오류가 발생한 거였음
 	SINGLETON(CSceneManager);
 
-
+	
 	CScene* m_arrScene[(int)SCENE_TYPE::Size];	//	모든 씬 목록
 	CScene* m_pCurScene;						//	현제 씬 목록
 
 public:
+
 	void update();
 	void render(HDC hDC);
 	void init();
